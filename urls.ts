acf254a02c4fa1b894c251730fa7406470ca64f1
apurbadh <erikduke182@gmail.com>
2021-10-18 19:38:45 +0545

    urls added

--- urls.ts
+++ urls.ts
@@ -1,4 +1,8 @@
-import { url } from "@apurbadh/cheetah";
+import { extendedURL, url } from "@apurbadh/cheetah";
 import HomeController from './controllers/HomeController';
+import PostsController from './controllers/PostsController';
+import PostController from './controllers/PostController';
 
 url("/", HomeController)
+url('/post', PostsController)
+extendedURL('/post/{id}', PostController)
